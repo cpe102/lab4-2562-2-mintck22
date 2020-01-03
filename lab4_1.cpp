fae4c7490f173a97821cee5201e5a4682dc929aa
Mint <chawanluck_k@hotmail.com>
2020-01-03 16:21:06 +0700

    lab4_1

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+#include<cmath>
 
+using namespace std;
+
+double funcY(double x){
+    return 3*x*x*x+2*2.71828+pow(2,2*x+1)+sqrt(x*x+1);
+}
+
+int main()
+{
+	double x;
+	cout << "Enter x = ";
+    cin >> x;
+
+    cout << "Result y = " << funcY(x);
+
+	
+	
+	return 0;
+}
