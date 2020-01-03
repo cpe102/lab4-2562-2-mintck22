cb977b110676eb876a7f706b046564156d1701e5
Mint <chawanluck_k@hotmail.com>
2020-01-03 16:23:41 +0700

    lab4_3

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,36 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int findDivisor(int i){
+    int j = 2;
+    while (j>0)
+    {
+        if (i%j == 0 )
+        {
+            return j;
+        }else{
+        	j++;
+		}
+        
+    }
+    
+}
+
+int main()
+{
+	int i;
+	cout << "Enter i = ";
+    cin >> i;
+    
+
+    cout << "result = " << findDivisor(i);
+
+
+
+	
+	return 0;
+}
+
 
